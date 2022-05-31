ba6de3f0c030749027b125590af786d9ced52b4a
Hatim M'rabet El Khomssi <mrabet.hatim2018@gmail.com>
2022-05-31 12:42:22 +0200

    edit product

--- src/main/java/com/lapetitesyrienne/api/controllers/ProduitController.java
+++ src/main/java/com/lapetitesyrienne/api/controllers/ProduitController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -107,10 +108,66 @@ public class ProduitController {
         return ResponseEntity.status(HttpStatus.CREATED).body(produit);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editProduit(@PathVariable String id,
+            @RequestParam(name="image", required=false) MultipartFile image,
+            @RequestParam("name") String name, @RequestParam("description") String description,
+            @RequestParam("price") double price, @RequestParam("categorie") String categorieName,
+            @RequestParam("ingredients") String[] ingredientsName) {
+        // get produit
+        if (!produitRepository.findById(id).isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage("Produit not found"));
+        }
+        Produit produit = produitRepository.findById(id).get();
+        // update produit
+        produit.setName(name);
+        produit.setDescription(description);
+        produit.setPrice(price);
+        // list des ingredients
+        Ingredient[] ingredients = new Ingredient[ingredientsName.length];
+        for (int i = 0; i < ingredientsName.length; i++) {
+            if (ingredientRepository.findByNameIgnoreCase(ingredientsName[i]) == null) {
+                Ingredient ingr = new Ingredient(ingredientsName[i]);
+                ingr.formatName();
+                ingredientRepository.save(ingr);
+            }
+            ingredients[i] = ingredientRepository.findByNameIgnoreCase(ingredientsName[i]);
+        }
+        produit.setIngredients(ingredients);
+        // get categorie
+        if (categorieRepository.findByNameIgnoreCase(categorieName) == null) {
+            Categorie cat = new Categorie(categorieName);
+            cat.formatName();
+            categorieRepository.save(cat);
+        }
+        produit.setCategorie(categorieRepository.findByNameIgnoreCase(categorieName));
+        // product image
+        if (image != null) {
+            // supprimer l'ancienne image
+            File oldImage = new File(root.getAbsolutePath() + "/" + produit.getImage());
+            oldImage.delete();
+            // ajouter la nouvelle image
+            produit.setImage(produit.getId() + image.getOriginalFilename());
+            // upload image
+            try {
+                File path = new File(root.getAbsolutePath() + "/" + produit.getImage());
+                path.createNewFile();
+                FileOutputStream output = new FileOutputStream(path);
+                output.write(image.getBytes());
+                output.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // enregistrer le produit
+        produitRepository.save(produit);
+        return ResponseEntity.ok().body(new ResponseMessage("Product updated"));
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteProduit(@PathVariable String id) {
         if (produitRepository.findById(id).isPresent()) {
-            //delete image
+            // delete image
             File image = new File(root.getAbsolutePath() + "/" + produitRepository.findById(id).get().getImage());
             image.delete();
             produitRepository.deleteById(id);
@@ -123,7 +180,7 @@ public class ProduitController {
     public ResponseEntity<?> getProduitGroupByCategorie() {
         HashMap<String, List<Produit>> map = new HashMap<>();
         List<Categorie> categories = categorieRepository.findAll();
-        for(Categorie categorie : categories) {
+        for (Categorie categorie : categories) {
             map.put(categorie.getName(), produitRepository.findByCategorie(categorie));
         }
         return ResponseEntity.status(HttpStatus.OK).body(map);
